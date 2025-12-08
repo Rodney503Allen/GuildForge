582a2b3036a526b2771f091a4912f0b63eaa2dde
Rodney503Allen <rodney.jay.allen@gmail.com>
2025-12-08 03:52:57 -0800

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,13 @@ import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
 });
